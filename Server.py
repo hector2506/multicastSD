bf991ce8ccf212c07969227f11c6aff3cd691c99
metangross <mettangroos@gmail.com>
2020-11-21 19:14:23 -0300

    Adicionado identificado unico

--- Server.py
+++ Server.py
@@ -2,13 +2,58 @@ import socket
 import struct
 import sys
 
-multicast_group = '224.2.2.3'
-server_address = ('', 8888)
 
 # Create the socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+# Heartbeat
+##############################################################################
+sock.settimeout(0.2)
+ttl = struct.pack('b', 1)
+sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, ttl)
+
+
+message = b'HEARTBEAT'
+multicast_group_servers = ('224.2.2.3', 8888)
+try:
+    sent = sock.sendto(message, multicast_group_servers)
+    # Look for responses from all recipients
+    cont = 0
+    listServers = []
+    while True:
+        #print('Encontrando servidores')
+        try:
+            data, server = sock.recvfrom(1024)
+            #print("Servidor Encontrado!")
+            cont+=1
+            listServers.append(int(data.decode("utf-8")))
+        except socket.timeout:
+            data, server = None, None
+            if cont is 0:
+                #print('Nenhum servidor ligado!')
+                id = 0
+            else:
+                #print("Mais nenhum servidor foi encontrado!")
+                #if cont is not 0:        
+                #print("ID dos servidores encontrados = {}".format(listServers))
+                i = 0
+                while i in listServers:
+                    i += 1
+                id = i
+
+            break
+        else:
+            print('received {!r} from {}'.format(
+                data, server))
+finally:
+    print("ID do servidor é {}".format(id))
+    sock.close()
+#############################################################################
+
 
 # Bind to the server address
+multicast_group = '224.2.2.3'
+server_address = ('', 8888)
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind(server_address)
 
 # Tell the operating system to add the socket to
@@ -27,14 +72,16 @@ while True:
 
     print('received {} bytes from {}'.format(
         len(data), address))
-    print(data.decode("utf-8"))     
-    try:
-        result = round(eval(data),3) 
-        print(f"O resultado da expressão é {result}\n" )
-        print(f'Enviando o resultado {result} para {address}')
-    except:
-        result = "Expressao Invalida"
-        print("Expressão Inválida recebida!")
-    message = bytes(str(result), encoding="ascii") 
+    if(data.decode("utf-8") == "HEARTBEAT"):
+        message = bytes(str(id),encoding="ascii")
+    else:
+        try:
+            result = round(eval(data),3) 
+            print("O resultado da expressão é {}\n".format(result))
+            print('Enviando o resultado {} para {}'.format(result, address))
+        except:
+            result = "Expressao Invalida"
+            print("Expressão Inválida recebida!")
+        message = bytes(str(result), encoding="ascii") 
     sock.sendto(message, address)
 
