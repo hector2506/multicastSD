83b537b182a6ac4addc6cecc559ecdc3436452a4
metangross <mettangroos@gmail.com>
2020-11-23 17:09:28 -0300

    Comentarios e prints atualizados

--- Client.py
+++ Client.py
@@ -1,4 +1,3 @@
-#socket_multicast_sender.py
 import socket
 import struct
 import sys
@@ -8,40 +7,25 @@ while eq is '':
     eq = input("Insira uma expressão: ")
     if eq is '':
         print("Expressão vazia inserida")
-
 message = bytes(eq, encoding="ascii")    
 multicast_group = ('224.2.2.3', 8888)
-
-# Create the datagram socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-# Set a timeout so the socket does not block
-# indefinitely when trying to receive data.
 sock.settimeout(0.2)
-
-# Set the time-to-live for messages to 1 so they do not
-# go past the local network segment.
 ttl = struct.pack('b', 1)
 sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, ttl)
-
 try:
-
-    # Send data to the multicast group
-    print('sending {!r}'.format(message.decode("utf-8")))
     sent = sock.sendto(message, multicast_group)
-
-    # Look for responses from all recipients
     while True:
-        print('waiting to receive')
+        print('Esperando para receber a resposta do servidor')
         try:
             data, server = sock.recvfrom(1024)
         except socket.timeout:
-            print('timed out, no more responses')
+            print('Nenhuma resposta recebida')
             break
         else:
-            print('received {!r} from {}'.format(
-                data, server))
-
+            print('Resultado da expressão: {}'.format(
+                data.decode("utf-8")))
+            break
 finally:
-    print('closing socket')
+    print('Encerrando a operação')
     sock.close()
--- Server.py
+++ Server.py
@@ -6,113 +6,75 @@ import time
 def Heartbeat(inicializado=False):
     while True:
         if inicializado is False:
-            time.sleep(5)
+            time.sleep(0.1)
         global id
         global listServers
-        # Create the socket
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        # Heartbeat
-        ##############################################################################
         sock.settimeout(0.2)
         ttl = struct.pack('b', 1)
         sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, ttl)
-
-
         message = b'HEARTBEAT'
         multicast_group_servers = ('224.2.2.3', 8888)
         try:
-            #print("Enviando HEARTBEAT")
             sent = sock.sendto(message, multicast_group_servers)
-            server_IP = socket.gethostbyname(socket.gethostname())
-            # Look for responses from all recipients
             listServers = []
             cont = 0
-            
             while True:
-                #print('Encontrando servidores')
                 try:
                     data, server = sock.recvfrom(1024)
-                    #print("Servidor Encontrado!")
                     cont+=1
-                    #print(data)
-                    #print("ID = {} e data = {}".format(id, data.decode("utf-8")))
                     if id != data.decode("utf-8"):
                         listServers.append(int(data.decode("utf-8")))
                 except socket.timeout:
                     if inicializado is True:        
                         data, server = None, None
                         if cont is 0:
-                            #print('Nenhum servidor ligado!')
                             id = 0
                         else:
-                            #print("Mais nenhum servidor foi encontrado!")
-                            #if cont is not 0:        
-                            #print("ID dos servidores encontrados = {}".format(listServers))
                             i = 0
                             while i in listServers:
                                 i += 1
                             id = i
-                    #listServers.append(id)
-                    break
-                else:
-                    if server_IP != server[0]:
-                        print('received {!r} from {}'.format(
-                          data, server))
+                    break        
         finally:
             if inicializado is True:
                 print("ID do servidor é {}".format(id))
-                break
-            if inicializado is False:
-                pass
-                #print("Lista de servidores = {}".format(listServers))
+                break                
             sock.close()
-    #############################################################################
 
 def Server():
     global id
     global listServers
-    # Bind to the server address
     multicast_group = '224.2.2.3'
     server_address = ('', 8888)
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.bind(server_address)
-
-    # Tell the operating system to add the socket to
-    # the multicast group on all interfaces.
     group = socket.inet_aton(multicast_group)
     mreq = struct.pack('4sL', group, socket.INADDR_ANY)
     sock.setsockopt(
         socket.IPPROTO_IP,
         socket.IP_ADD_MEMBERSHIP,
         mreq)
-    print('waiting to receive message')
-    # Receive/respond loop
-    while True:
+    print('Esperando receber mensagem') 
+    while True:       
         data, address = sock.recvfrom(1024)
-        server_IP = socket.gethostbyname(socket.gethostname())
         host_IP = address[0]
-        if server_IP != host_IP:
-            print('received {} bytes from {}'.format(
-                len(data), address))
-        #print(data)
         if(data.decode("utf-8") == "HEARTBEAT"):
             message = bytes(str(id),encoding="ascii")
             sock.sendto(message, address)
         else:
+            print('Recebido a expressão {} de {}'.format(
+                data.decode("utf-8"), address[0]))
             listServers.sort()
-            #print(listServers)
             if id == listServers[0]:
                 try:
                     result = round(eval(data),3) 
-                    #print("O resultado da expressão é {}\n".format(result))
-                    print('Enviando o resultado {} para {}'.format(result, address))
+                    print('Enviando o resultado {} para {}'.format(result, address[0]))
                 except:
                     result = "Expressao Invalida"
                     print("Expressão Inválida recebida!")
                 message = bytes(str(result), encoding="ascii") 
                 sock.sendto(message, address)
-
-
 Heartbeat(True)
 threads = []
 Serv = threading.Thread(target=Server)
@@ -123,7 +85,3 @@ Verif.start()
 threads.append(Verif)
 for thread in threads:
     thread.join()
-#Verif.join()
-#Server()
-#Heartbeat(False)
-#Server()
